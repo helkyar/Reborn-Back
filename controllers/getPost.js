0618d662cf84ae2e7117855a1e1b80ff993c2258
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-02-26 12:12:22 +0100

    Fix routing

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -2,7 +2,7 @@ const data = require("../resources/mockdata");
 
 function getPost(req, res) {
   //db connection get data
-  res.status(200).json(data.members);
+  res.status(200).json(data);
 }
 
 module.exports = getPost;
--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
-// router.{get/post}('', require(''));
 router.get("/:id", require("../controllers/getPost"));
-router.post("", require("../controllers/putPost"));
+router.post("/", require("../controllers/putPost"));
+// router.get("/", require("../controllers/putPost"));
 
 module.exports = router;
